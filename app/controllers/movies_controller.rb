0aca6c5bcd574dc33908208b17d136fc39741c3a
Kimleng <kimlenghor2006@gmail.com>
2023-02-21 09:11:30 -0600

    add filter functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,11 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all
+    if params[:rating] 
+      @movies = Movie.select { |mov| mov.rating == params[:rating][:filter] }
+    else 
+      @movies = Movie.order('release_date DESC')
+    end
   end
 
   # GET /movies/1 or /movies/1.json
